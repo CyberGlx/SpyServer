cf2736055d033498f20a0d596403a26ea37f8364
CyberGlx <cyberrrgirlsss@gmail.com>
2025-06-04 19:47:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,4 +24,4 @@ def upload_file():
     return jsonify({'status': 'success', 'filename': file.filename}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=3000)
